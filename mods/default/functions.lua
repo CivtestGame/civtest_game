0eacec1c8ff3cfc009a3cbc5ec875af690f444c0
R3R3R3 <>
2020-07-24 21:28:51 +0200

    Cooled lava produces stone with param2==1

--- mods/default/functions.lua
+++ mods/default/functions.lua
@@ -138,7 +138,8 @@ default.cool_lava = function(pos, node)
 	if node.name == "default:lava_source" then
 		minetest.set_node(pos, {name = "default:obsidian"})
 	else -- Lava flowing
-		minetest.set_node(pos, {name = "default:stone"})
+		minetest.set_node(pos, {name = "default:stone", param2 = 1})
+
 	end
 	minetest.sound_play("default_cool_lava",
 		{pos = pos, max_hear_distance = 16, gain = 0.25}, true)
