dfa452770f5726d788b5bed7d8c1806dc4d7f5f8
R3R3R3 <>
2020-08-19 20:17:44 +0200

    Add Sticky Water node definitions

--- mods/default/nodes.lua
+++ mods/default/nodes.lua
@@ -1948,6 +1948,97 @@ minetest.register_node("default:water_source", {
 	sounds = default.node_sound_water_defaults(),
 })
 
+minetest.register_node("default:sticky_water_source", {
+	description = "Sticky Water Source",
+	drawtype = "liquid",
+	tiles = {
+		{
+			name = "default_water_source_animated.png^[brighten",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+		{
+			name = "default_water_source_animated.png^[brighten",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 2.0,
+			},
+		},
+	},
+	alpha = 200,
+	paramtype = "light",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "source",
+	liquid_alternative_flowing = "default:sticky_water_flowing",
+	liquid_alternative_source = "default:sticky_water_source",
+	liquid_viscosity = 7,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {water = 3, liquid = 3, cools_lava = 1},
+	sounds = default.node_sound_water_defaults(),
+})
+
+
+minetest.register_node("default:sticky_water_flowing", {
+	description = "Flowing Sticky Water",
+	drawtype = "flowingliquid",
+	tiles = {"default_water.png"},
+	special_tiles = {
+		{
+			name = "default_water_flowing_animated.png^[brighten",
+			backface_culling = false,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.8,
+			},
+		},
+		{
+			name = "default_water_flowing_animated.png^[brighten",
+			backface_culling = true,
+			animation = {
+				type = "vertical_frames",
+				aspect_w = 16,
+				aspect_h = 16,
+				length = 0.8,
+			},
+		},
+	},
+	alpha = 200,
+	paramtype = "light",
+	paramtype2 = "flowingliquid",
+	walkable = false,
+	pointable = false,
+	diggable = false,
+	buildable_to = true,
+	is_ground_content = false,
+	drop = "",
+	drowning = 1,
+	liquidtype = "flowing",
+	liquid_alternative_flowing = "default:sticky_water_flowing",
+	liquid_alternative_source = "default:sticky_water_source",
+	liquid_viscosity = 7,
+	post_effect_color = {a = 103, r = 30, g = 60, b = 90},
+	groups = {water = 3, liquid = 3, not_in_creative_inventory = 1,
+		cools_lava = 1},
+	sounds = default.node_sound_water_defaults(),
+})
+
+
 minetest.register_node("default:water_flowing", {
 	description = "Flowing Water",
 	drawtype = "flowingliquid",
